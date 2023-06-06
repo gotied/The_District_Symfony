3c5e814cd3abb40b9d6e0fa9eaaac059f88c6de8
gotied <gautierdelbet80@gmail.com>
2023-06-06 16:49:29 +0200

    0.0.1

--- sql/the_district.sql
+++ sql/the_district.sql
@@ -1,52 +1,80 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           10.6.12-MariaDB-0ubuntu0.22.04.1 - Ubuntu 22.04
+-- SE du serveur:                debian-linux-gnu
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE IF NOT EXISTS `the_district` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+-- Listage de la structure de la base pour the_district
+CREATE DATABASE IF NOT EXISTS `the_district` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `the_district`;
 
-DROP TABLE IF EXISTS `categorie`;
+-- Listage de la structure de table the_district. categorie
 CREATE TABLE IF NOT EXISTS `categorie` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `libelle` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `commande`;
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table the_district. commande
 CREATE TABLE IF NOT EXISTS `commande` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `utilisateur_id` int DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `utilisateur_id` int(11) DEFAULT NULL,
   `date_commande` date NOT NULL,
   `total` decimal(6,2) NOT NULL,
-  `etat` int NOT NULL,
+  `etat` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_6EEAA67DFB88E14F` (`utilisateur_id`)
+  KEY `IDX_6EEAA67DFB88E14F` (`utilisateur_id`),
+  CONSTRAINT `FK_6EEAA67DFB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `detail`;
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table the_district. detail
 CREATE TABLE IF NOT EXISTS `detail` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `plat_id` int DEFAULT NULL,
-  `commande_id` int DEFAULT NULL,
-  `quantite` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plat_id` int(11) DEFAULT NULL,
+  `commande_id` int(11) DEFAULT NULL,
+  `quantite` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_2E067F93D73DB560` (`plat_id`),
-  KEY `IDX_2E067F9382EA2E54` (`commande_id`)
+  KEY `IDX_2E067F9382EA2E54` (`commande_id`),
+  CONSTRAINT `FK_2E067F9382EA2E54` FOREIGN KEY (`commande_id`) REFERENCES `commande` (`id`),
+  CONSTRAINT `FK_2E067F93D73DB560` FOREIGN KEY (`plat_id`) REFERENCES `plat` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `messenger_messages`;
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table the_district. doctrine_migration_versions
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table the_district. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `body` longtext NOT NULL,
+  `headers` longtext NOT NULL,
+  `queue_name` varchar(190) NOT NULL,
   `created_at` datetime NOT NULL,
   `available_at` datetime NOT NULL,
   `delivered_at` datetime DEFAULT NULL,
@@ -56,46 +84,43 @@ CREATE TABLE IF NOT EXISTS `messenger_messages` (
   KEY `IDX_75EA56E016BA31DB` (`delivered_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `plat`;
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table the_district. plat
 CREATE TABLE IF NOT EXISTS `plat` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `categorie_id` int DEFAULT NULL,
-  `libelle` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categorie_id` int(11) DEFAULT NULL,
+  `libelle` varchar(50) NOT NULL,
+  `description` longtext NOT NULL,
   `prix` decimal(6,2) NOT NULL,
-  `image` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(50) NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_2038A207BCF5E72D` (`categorie_id`)
+  KEY `IDX_2038A207BCF5E72D` (`categorie_id`),
+  CONSTRAINT `FK_2038A207BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `utilisateur`;
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table the_district. utilisateur
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prenom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telephone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `adresse` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cp` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ville` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `roles` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `telephone` varchar(20) NOT NULL,
+  `adresse` varchar(50) NOT NULL,
+  `cp` varchar(20) NOT NULL,
+  `ville` varchar(50) NOT NULL,
+  `roles` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- Les données exportées n'étaient pas sélectionnées.
 
-ALTER TABLE `commande`
-  ADD CONSTRAINT `FK_6EEAA67DFB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`);
-
-ALTER TABLE `detail`
-  ADD CONSTRAINT `FK_2E067F9382EA2E54` FOREIGN KEY (`commande_id`) REFERENCES `commande` (`id`),
-  ADD CONSTRAINT `FK_2E067F93D73DB560` FOREIGN KEY (`plat_id`) REFERENCES `plat` (`id`);
-
-ALTER TABLE `plat`
-  ADD CONSTRAINT `FK_2038A207BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
-COMMIT;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- src/DataFixtures/Jeu1.php
+++ src/DataFixtures/Jeu1.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Categorie;
+use App\Entity\Plat;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -18,5 +19,14 @@ class Jeu1 extends Fixture
          $manager->persist($categorie1);
 
          $manager->flush();
+
+         $plat1 = new Plat();
+         $plat1->setLibelle("Pizza Bianca");
+         $plat1->setDescription("Une pizza fine et croustillante garnie de crème mascarpone légèrement citronnée et de tranches de saumon fumé, le tout relevé de baies roses et de basilic frais.");
+         $plat1->setPrix(14.00);
+         $plat1->setImage("pizza-salmon.png");
+         $plat1->setActive(true);
+        
+         $categorie1->addPlat($plat1);
     }
 }
