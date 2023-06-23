3ca55c538b66933a5c5f1360bd15ff454e289ce5
gotied <gautierdelbet80@gmail.com>
2023-06-23 23:13:26 +0200

    0.0.5/3

--- public/js/script.js
+++ public/js/script.js
@@ -26,3 +26,21 @@ function modifierQuantite(platId, quantite) {
             console.error(error);
         });
 }
+function updateTotalPrice() {
+    var total = 0;
+    var selects = document.getElementsByTagName("select");
+    
+    for (var i = 0; i < selects.length; i++) {
+      var select = selects[i];
+      var price = parseFloat(select.parentNode.parentNode.querySelector('.card-text').textContent.trim());
+      var quantity = parseInt(select.value);
+      total += price * quantity;
+    }
+    document.getElementById("total").value = total.toFixed(2) + " €";
+  }
+  window.addEventListener("load", updateTotalPrice);
+  var selects = document.getElementsByTagName("select");
+  for (var i = 0; i < selects.length; i++) {
+    selects[i].addEventListener("change", updateTotalPrice);
+  }
+  
