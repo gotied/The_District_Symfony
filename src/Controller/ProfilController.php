9a9890db4b8ba14165be0502856236ab6afcb07c
gotied <gautierdelbet80@gmail.com>
2023-06-26 16:50:01 +0200

    0.0.5/4

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CommandeRepository;
+use App\Repository\DetailRepository;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(CommandeRepository $commande, DetailRepository $detail): Response
     {
+        $co = $commande->findAll();
+        $de = $detail->findAll();
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
+            'commande' => $co,
+            'detail' => $de
         ]);
     }
 }
