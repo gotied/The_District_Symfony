2647a6e287da0fe9384ed38c52ac20861276a21d
gotied <gautierdelbet80@gmail.com>
2023-07-12 16:45:50 +0200

    0.0.5/7

--- src/EventSubscriber/CommandeSubscriber.php
+++ src/EventSubscriber/CommandeSubscriber.php
@@ -17,6 +17,7 @@ class CommandeSubscriber implements EventSubscriber
     private $mailer;
     private $detailRepository;
     private $commandeRepository;
+    private $sendCO = [];
 
     public function __construct(MailerInterface $mailer, DetailRepository $detailRepository, CommandeRepository $commandeRepository)
     {
@@ -35,33 +36,14 @@ class CommandeSubscriber implements EventSubscriber
     public function postPersist(LifecycleEventArgs $args)
     {
         $entity = $args->getObject();
-        $entity1 = $args->getObject();
 
         if ($entity instanceof \App\Entity\Detail) {
-                // $user = $entity->getUtilisateur()->getEmail();
-                // $date = $entity->getDateCommande();
-                // $total = $entity->getTotal();
-                // $id = $entity->getId();
-                
-                // $detail = $this->detailRepository->findBy(['commande' => $id]);
-                // $detail = $this->detailRepository->findAll();
-                // $message = 'Commande : ' . PHP_EOL;
-                // foreach ($detail as $d) {
-                //     if ($d->getCommande()->getId() == $id) {
-                //         $quantite = $d->getQuantite();
-                //         $libelle = $d->getPlat();
-
-                //         $message .= $libelle . ' (Quantité : ' . $quantite . ')' . PHP_EOL;
-                //     }
-                // }
-                // $detail = $entity->getDetails();
-
-                // $quantite = $detail->getQuantite();
-                // $plat = $detail->getPlat();
-
                 $commandeID = $entity->getCommande()->getId();
                 $commande = $this->commandeRepository->findAll();
                 $detail = $this->detailRepository->findAll();
+                if (in_array($commandeID, $this->sendCO)) {
+                    return;
+                }
                 foreach ($commande as $c) {
                     $coID = $c->getId();
                     if ($coID === $commandeID) {
@@ -92,8 +74,9 @@ class CommandeSubscriber implements EventSubscriber
                 $message .= $adresse . PHP_EOL;
                 $message .= $ville . PHP_EOL;
                 $message .= $cp . PHP_EOL . PHP_EOL;
+                $message .= "Nous vous tiendrons informés de l'état de votre commande et de sa livraison !" . PHP_EOL;
                 $message .= 'À bientôt sur The District.';
-                
+
                 $email = (new Email())
                     ->from('commande@the_district.fr')
                     ->to($user)
@@ -101,6 +84,34 @@ class CommandeSubscriber implements EventSubscriber
                     ->text($message);
 
                 $this->mailer->send($email);
+
+                $text = 'Une nouvelle commande a été passée.' . PHP_EOL . PHP_EOL;
+                $text .= $user . PHP_EOL;
+                $text .= $tel . PHP_EOL . PHP_EOL;
+                $text .= 'Adresse de livraison :' . PHP_EOL;
+                $text .= $nom . ' ' . $prenom . PHP_EOL;
+                $text .= $adresse . PHP_EOL;
+                $text .= $ville . ' ' . $cp . PHP_EOL . PHP_EOL; 
+                $text .= 'Détails de la commande :' . PHP_EOL;
+                foreach ($detail as $d) {
+                    if ($commandeID === $d->getCommande()->getId()) {
+                        $quantite = $d->getQuantite();
+                        $libelle = $d->getPlat()->getLibelle();
+
+                        $text .= $libelle . ' (Quantité : ' . $quantite . ')' . PHP_EOL; 
+                    }
+                }
+                $text .= 'Total : ' . $total . '€';
+
+                $email = (new Email())
+                    ->from('commande@the_district.fr')
+                    ->to('admin@the_district.fr')
+                    ->subject('Nouvelle commande - Récapitulatif')
+                    ->text($text);
+                
+                $this->mailer->send($email);
+
+                $this->sendCO[] = $commandeID;
         }
     }
 }
