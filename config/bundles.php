635e4cfbf66af52747b4d3c81f49a43277b5d193
gotied <gautierdelbet80@gmail.com>
2023-07-18 14:03:50 +0200

    0.0.5/9

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
